6b2ffd0f59bff8579ca9f14712a7f73b1a14b11e
Clarisse Lima <clarisse@csktech.com.br>
2019-05-10 10:19:07 -0300

    First structure of the tables

---
+++ database/migrations/2019_05_10_125520_create_participants_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateParticipantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->integer('age');
+            $table->date('birth');
+            $table->unsignedBigInteger('groups_id');
+            $table->foreign('groups_id')->references('id')->on('groups');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participants');
+    }
+}
---
+++ database/migrations/2019_05_10_125825_create_presences_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePresencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('presences', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('participants_id');
+            $table->foreign('participants_id')->references('id')->on('participants');
+            $table->unsignedBigInteger('meetings_id');
+            $table->foreign('meetings_id')->references('id')->on('meetings');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('presences');
+    }
+}
